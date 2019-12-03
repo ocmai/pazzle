57ad4b0b1bae08206c02feeb2baa3445d6b6fedf
ocmai <saki04mai@gmail.com>
2019-12-03 16:24:21 +0900

    Update

--- q01.js
+++ q01.js
@@ -1,7 +1,11 @@
 //文字列型に逆順に返すメソッドを追加
 String.prototype.reverse = function(){
   return this.split("").reverse().join("");
+  //splitで1文字ずつに分割する
+  //reverseで逆順にする
+  //joinで逆順にしたものをくっつける
 }
+
 //11から探索
 var num = 11;
 while(true){
@@ -10,6 +14,8 @@ while(true){
     (num.toString(2) == num.toString(2).reverse())){
       console.log(num);
       break;
+      //toString(n)でn真数に変換する
+      //&&は「かつ」
     }
     //奇数だけを探すため2つずつ増やす
     num += 2;
